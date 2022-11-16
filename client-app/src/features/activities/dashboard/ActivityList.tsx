9eacfbe9776b678c02810f96c718d5549def64fa
Teddy <teddygirma20@gmail.com>
2022-11-16 17:41:00 +0300

    Complete Frontend-CRUD

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Button, Item, Label, Segment } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+
+interface Props{
+    activities:Activity[];
+  selectActivity: (id: string) => void;
+  deleteActivity: (id:string) => void;
+
+}
+
+const ActivityList = ({activities, selectActivity, deleteActivity}: Props) => {
+  return (
+    <Segment>
+        <Item.Group>
+            {activities.map(activity => (
+                <Item key={activity.id}>
+                   <Item.Content>
+                       <Item.Header>{activity.title}</Item.Header>
+                       <Item.Meta>{activity.date}</Item.Meta>
+                       <Item.Description>
+                          <div>{activity.description}</div>
+                          <div>{activity.city}, {activity.category}</div>
+                       </Item.Description>
+                       <Item.Extra>
+                          <Button floated='right' content='View' color='blue' onClick={() =>selectActivity(activity.id) } />
+                          <Button floated='right' content='Delete' color='red' onClick={() =>deleteActivity(activity.id) } />
+                          <Label basic content={activity.category} />
+                       </Item.Extra>
+                   </Item.Content>
+                </Item>
+            ))}
+        </Item.Group>
+  </Segment>
+  )
+}
+
+export default ActivityList
